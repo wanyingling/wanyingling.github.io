02373c6f3e093cf021a619cc706cc60a36b09396
Wanying Ling <gufanling@outlook.com>
2026-02-12 09:31:33 +0800

    Update All

---
+++ astro.config.mjs
@@ -0,0 +1,61 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import remarkPrism from 'remark-prism'
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import remarkToc from 'remark-toc'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://github.com/wanyingling/wanyingling.github.io',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  // https://github.com/natemoo-re/astro-icon/issues/2
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
+  markdown: {
+    remarkPlugins: [
+      [remarkToc, { tight: true, ordered: false }],
+      remarkMath,
+      remarkGfm,
+      remarkReadingTime,
+      [
+        remarkPrism,
+        {
+          plugins: ['line-numbers'],
+          transformInlineCode: false
+        }
+      ]
+    ],
+    rehypePlugins: [
+      [
+        rehypeKatex,
+        {
+          // Katex plugin options
+          macros: {
+            '\\E': '\\mathbb{E}',
+            '\\C': '\\mathbb{C}',
+            '\\R': '\\mathbb{R}',
+            '\\N': '\\mathbb{N}',
+            '\\Q': '\\mathbb{Q}',
+            '\\bigO': '\\mathcal{O}',
+            '\\abs': '|#1|',
+            '\\set': '\\{ #1 \\}',
+            '\\indep': '{\\perp\\mkern-9.5mu\\perp}',
+            '\\nindep': '{\\not\\!\\perp\\!\\!\\!\\perp}',
+            '\\latex': '\\LaTeX',
+            '\\katex': '\\KaTeX'
+          }
+        }
+      ]
+    ],
+    syntaxHighlight: false
+  }
+})
---
+++ src/pages/reading-list/index.md
@@ -0,0 +1,18 @@
+---
+layout: '../../layouts/BlogPost.astro'
+title: Reading List
+noReadingTime: true
+---
+
+This is a curated reading list of mine.
+
+- [2025](/reading-list/2025) - My favourite articles or books
+- [2024](/reading-list/2024) - My favourite articles or books
+- [2023](/reading-list/2023) - My favourite articles or books
+- [2022](/reading-list/2022) - My favourite articles or books
+- [2021](/reading-list/2021) - My favourite articles or books
+- [2020](/reading-list/2020) - My favourite articles or books
+
+<br>
+
+> We stand on the shoulders of giants.
